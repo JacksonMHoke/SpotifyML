f67e1cac256a7142c9d84505ef6a9de9a751653b
JacksonMHoke <jacksonmhoke@gmail.com>
2021-07-04 00:27:49 -0700

    Starting to implement calcDistance
    - Began implementation of calcDistance
    - Created a normalize function and a code exerpt in featureSearch()
      to normalize data in csv so distances can be calculated with better
      weights
    - fixed a bug where there would only be one iteration of featuresearch
      thus creating a max of 1 feature

--- featuresearch.py
+++ featuresearch.py
@@ -2,6 +2,22 @@ from os import replace
 import pandas as pd
 import numpy as np
 import sys
+import heapq
+import random
+
+''' 
+TODO: Algorithm does not work currently because there are features that have near complete uniformitivity.
+      Create a way to find a good set of features to base the model off of(unsupervised clustering evaluation model)
+'''
+
+def normalize(input, max):
+    '''
+    normalize: Adds weights to the data so that the columns portray similar values
+
+    Parameters:
+        - playlistData - Dataframe of all the playlist data from playlist.csv
+    '''
+    return input/max
 
 def calcDistance(playlistData, trackID1, trackID2, features):
     '''
@@ -13,8 +29,16 @@ def calcDistance(playlistData, trackID1, trackID2, features):
         - trackID2 - Track ID of the second track
         - features - list of features to compare distance on
     '''
-    return 0
-
+    row1=playlistData.loc[playlistData['Track']==trackID1]
+    row2=playlistData.loc[playlistData['Track']==trackID2] #TODO: MUST MAKE IT SO row2.at[0, feature] accesses the correct index
+    dist=0
+    for feature in features:
+        print("ROW1 FEATURE: ", row1.at[0, feature], type(row1.at[0, feature]))
+        print("ROW2 FEATURE: ", row2, type(row2))
+        dist+=(row1.at[0, feature]-row2.at[0, feature])**2
+        print(dist)
+    print("CALCDISTANCE TYPE: ", type(dist))
+    return dist**.5
 
 def evaluate(playlistData, songData, features, numNeighbors, trackIDS=pd.DataFrame()):
     '''
@@ -30,9 +54,21 @@ def evaluate(playlistData, songData, features, numNeighbors, trackIDS=pd.DataFra
     Return Value:
         Returns average distance of numNeighbors closest playlist songs according to features passed in
     '''
+    neighbors=[] #All values will be negative in this to imitate a maxheap
+    for track in playlistData.itertuples():
+        #track[1] is the row for trackid
+        if track[1] not in trackIDS.itertuples():
+            dist=calcDistance(playlistData, track[1], songData[1], features)
+            if len(neighbors)<numNeighbors:
+                print(type(dist))
+                heapq.heappush(neighbors, -1*dist)
+            elif heapq.nsmallest(1, neighbors)[0] < -1*dist:
+                heapq.heappop(neighbors)
+                heapq.heappush(neighbors, -1*dist)
+    print("Returning from evaluate: ", sum(neighbors[0:len(neighbors)])*-1/numNeighbors)
+    return sum(neighbors[0:len(neighbors)])*-1/numNeighbors
 
-
-def validate(playlistData, features, sampleSize, iterations, numNeighbors):
+def validate(playlistData, features, sampleSize=2, iterations=2, numNeighbors=3):
     '''
     validate: Returns a score based on the features and playlistData passed in
 
@@ -45,11 +81,14 @@ def validate(playlistData, features, sampleSize, iterations, numNeighbors):
     '''
     sumScore=0
     for i in range(iterations):
+        print("iteration: ", i)
         score=0
         excludeRows=playlistData.sample(n=sampleSize, replace=True)
-        for row in excludeRows:
+        for row in excludeRows.itertuples():
             score+=evaluate(playlistData=playlistData, songData=row, features=features, numNeighbors=numNeighbors, trackIDS=excludeRows)
+            print("After updating row score now is: ", score)
         sumScore+=score/sampleSize
+    print("Returning from validate: ", sumScore/iterations)
     return sumScore/iterations
 
 def featureSearch():
@@ -60,30 +99,43 @@ def featureSearch():
 
     Return: An efficient list of features for the evaluate function
     '''
+
     #reading playlist data
     try:
         playlistData = pd.read_csv('playlist.csv')
     except:
         print('Playlist.csv does not exist yet, run PlaylistCrawler.py to create it.')
         quit()
-    features = list(playlistData.columns.values)
 
+    #normalizing all data so that numbers are ~0-1.5ish
+    for (columnName, columnData) in playlistData.iteritems():
+        if columnName != 'Genres' and columnName != 'Track':
+            playlistData[columnName]=playlistData[columnName].astype(float)
+            max=sum(columnData.nlargest(50))/50
+            playlistData[columnName]=playlistData[columnName].apply(normalize, args=(max, ))
+            print("MEAN OF ", columnName, ": " ,playlistData[columnName].mean())
+    print(playlistData)
+    
+    features = list(playlistData.columns.values[2:])
     #variables for feature search
-    minScore=sys.maxint
-    currScore=sys.maxint-1
+    minScore=10**25
+    currScore=10**25
     currFeatures=[]
     minFeature=''
 
     #choose the single feature that will minimize score
     #repeat until no features can decrease minScore
-    while minScore>currScore:
+    while minScore>=currScore:
         currScore=minScore
 
         #finding the feature that decreases score the most
         for col in features:
+            print("Testing column: ", col)
             currFeatures.append(col)
             score=validate(playlistData, currFeatures)
+            print("Score calculated for tha column is: ", score)
             if currScore>score:
+                print("New best score found for column: ", col)
                 currScore=score
                 minFeature=col
             currFeatures.remove(col)
@@ -92,8 +144,11 @@ def featureSearch():
             minScore=currScore
             currFeatures.append(minFeature)
             features.remove(minFeature)
+            print("new features found: ", currFeatures)
         else:
+            print("No better option found")
             break
 
     return currFeatures
     
+print(featureSearch())
